ad45245bf4875502e47da35158ee4888f090c540
Prince Vuha <princevuha@yahoo.com>
2025-10-24 15:21:17 +0000

    fix manifest fetching issues

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -5,7 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#0070f3" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="CineSeek" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
       </Head>
       <body className="antialiased">
         <Main />
